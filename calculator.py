4ccb5ab4bd7ab353b4c9a12b66a59f94af074a62
j1003 <pfeil.henry@ufl.edu>
2025-11-18 20:38:05 -0500

    git add calculator.py git commit -m "modified calculator" git push

--- calculator.py
+++ calculator.py
@@ -10,7 +10,6 @@ One function per operation, in order.
 
 import math
 
-
 def square_root(a):
     if a < 0:
         raise ValueError("square_root is undefined for negative numbers.")
@@ -27,7 +26,6 @@ def subtract(a,b):
 
 def mul(a,b):
     return a*b
-
 
 def div(a,b):
     if a==0:
